2298b76e3babd39fb0e032145787916d580437e2
Floyda <Floyda@163.com>
2019-10-24 20:07:21 +0800

    check whether filename is None

--- LuaFormatter.py
+++ LuaFormatter.py
@@ -11,11 +11,16 @@ class LuaFormatCommand(sublime_plugin.TextCommand):
         if file_suffix[-3:].lower() != "lua":
             return
 
-        self.filename = self.view.file_name()
+        # check whether filename is None
+        filename = self.view.file_name()
+        if filename is None:
+            return
+
+        # run lua-format
         package_path = os.path.split(os.path.dirname(__file__))[1]
         command = os.path.join(
             sublime.packages_path(), package_path, "bin", sys.platform, "lua-format"
         )
 
         lua_style = os.path.join(os.path.dirname(__file__), "lua_style")
-        process = subprocess.Popen([command, self.filename, "-c", lua_style])
+        process = subprocess.Popen([command, filename, "-c", lua_style])
